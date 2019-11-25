6df393440c7613564f3ff5b69cc400a06e4c5b63
milinsky <mail@milinsky.ru>
2019-11-25 22:27:19 +1000

    add AbstractAction

--- src/Actions/GetContentByIdAction.php
+++ src/Actions/GetContentByIdAction.php
@@ -4,47 +4,47 @@ declare(strict_types=1);
 
 namespace Marussia\Content\Actions;
 
-use Marussia\Content\RepositoryBundle;
-use Marussia\Content\Actions\Providers\GetContentByIdProvider;
+use Marussia\Content\RepositoryBundle as Repository;
+use Marussia\Content\Actions\Providers\GetContentByIdProvider as ActionProvider;
 use Marussia\Content\ViewModels\Content;
 use Marussia\Content\ContentBuilder;
 
-class GetContentByIdAction
+class GetContentByIdAction extends AbstractAction
 {
-    protected $repositoryBundle;
+    protected $repository;
 
-    protected $getContentByIdProvider;
+    protected $actionProvider;
 
     protected $contentBuilder;
 
-    public function __construct(RepositoryBundle $repositoryBundle, GetContentByIdProvider $getContentByIdProvider, ContentBuilder $contentBuilder)
+    public function __construct(Repository $repository, ActionProvider $actionProvider, ContentBuilder $contentBuilder)
     {
-        $this->repositoryBundle = $repositoryBundle;
-        $this->getContentByIdProvider = $getContentByIdProvider;
+        $this->repository = $repository;
+        $this->actionProvider = $actionProvider;
         $this->contentBuilder = $contentBuilder;
     }
 
     public function execute(string $contentTypeName, int $contentId) : Content
     {
-        $contentType = $this->repositoryBundle->getContentType($contentTypeName);
+        $contentType = $this->repository->getContentType($contentTypeName);
 
         if ($contentType === null) {
             throw new ContentTypeNotFoundException($contentTypeName);
         }
 
-        $fields = $this->repositoryBundle->getFields($contentTypeName);
-        $fieldsValues = $this->repositoryBundle->getFieldsValues($contentTypeName, $contentId);
+        $fields = $this->repository->getFields($contentTypeName);
+        $fieldsValues = $this->repository->getFieldsValues($contentTypeName, $contentId, $this->language);
 
         $contentData = [];
 
         foreach ($fieldsValues as $fieldType => $value) {
             if ($fields->has($fieldType)) {
-                $fieldData = $this->getContentByIdProvider->createFieldData($fields->get($fieldType));
+                $fieldData = $this->actionProvider->createFieldData($fields->get($fieldType));
                 $fieldData->value = $value;
-                $contentData[$fieldType] = $this->getContentByIdProvider->fillField($fieldData);
+                $contentData[$fieldType] = $this->actionProvider->fillField($fieldData);
                 continue;
             }
-            $contentData[$fieldType] = $this->getContentByIdProvider->createFieldWithoutHandler($value);
+            $contentData[$fieldType] = $this->actionProvider->createFieldWithoutHandler($value);
         }
 
         return $this->contentBuilder->createContent($contentData);
