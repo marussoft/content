6e4acd64e41154c7d3393635f6fbc0cbc95ce910
milinsky <mail@milinsky.ru>
2019-11-27 14:21:35 +1000

    fix composer.json

--- tests/unit/GetContentByIdActionTest.php
+++ tests/unit/GetContentByIdActionTest.php
@@ -5,15 +5,12 @@ declare(strict_types=1);
 namespace Marussia\Content\Test;
 
 use Marussia\Content\Actions\GetContentByIdAction;
-use Marussia\Content\Actions\AbstractAction;
 use Marussia\Content\RepositoryBundle;
 use Marussia\Content\Actions\Providers\GetContentByIdProvider;
 use Marussia\Content\ViewModels\Content;
 use Marussia\Content\ContentBuilder;
 use Marussia\Content\Entities\ContentType;
 use Marussia\Content\Collection;
-use Marussia\ContentField\Actions\FillAction;
-use Marussia\ContentField\FieldDataFactory;
 use Marussia\ContentField\Field;
 use Marussia\ContentField\FieldData;
 use PHPUnit\Framework\TestCase;
