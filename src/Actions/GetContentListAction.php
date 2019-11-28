e377fe67091bcf2b685dcc8ec3b07defbe7c4d62
milinsky <mail@milinsky.ru>
2019-11-28 18:18:18 +1000

    add filter method

--- src/Actions/GetContentListAction.php
+++ src/Actions/GetContentListAction.php
@@ -6,17 +6,44 @@ namespace Marussia\Content\Actions;
 
 use Marussia\Content\RepositoryBundle;
 use Marussia\Content\Actions\Providers\GetContentListProvider;
+use Marussia\Content\ViewModels\ContentList;
+use Marussia\Content\ContentBuilder;
 
 class GetContentListAction extends AbstractAction
 {
-    public function __construct(RepositoryBundle $repository, GetContentListProvider $actionProvider)
+    protected $repository;
+
+    protected $actionProvider;
+
+    protected $contentBuilder;
+
+    public function __construct(RepositoryBundle $repository, GetContentListProvider $actionProvider, ContentBuilder $contentBuilder)
     {
         $this->repository = $repository;
         $this->actionProvider = $actionProvider;
+        $this->contentBuilder = $contentBuilder;
     }
     
-    public function execute()
+    public function execute(string $contentTypeName) : ContentList
     {
+        $contentType = $this->repository->getContentType($contentTypeName);
+
+        if ($contentType === null) {
+            throw new ContentTypeNotFoundException($contentTypeName);
+        }
         
+        $options = $contentType->options;
+        $fields = $this->repository->getFields($contentTypeName);
+        $fieldsValues = $this->repository->getFieldsValuesList($contentTypeName, $options['pagination'], $this->language);
+
+        $contentData = [];
+        
+        foreach () {}
+    }
+    
+    protected function filter(array $filter) : self
+    {
+        $this->filter = $filter;
+        return $this;
     }
 }
