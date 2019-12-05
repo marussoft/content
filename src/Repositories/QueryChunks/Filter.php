7469ce1afd63fb73218482c770cf3b41613df031
milinsky <mail@milinsky.ru>
2019-12-05 11:00:40 +1000

    query chunk filter

--- src/Repositories/QueryChunks/Filter.php
+++ src/Repositories/QueryChunks/Filter.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace Marussia\Content\Repositories\QueqyChunks;
 
+use Marussia\ContentField\Actions\GetFilterParamsAction;
+
 class Filter
 {
-    publuc function getQueryString(array $params) : string
+    private $getFilterParams;
+    
+    public function __construct(GetFilterParamsAction $getFilterParams)
     {
+        $this->getFilterParams = $getFilterParams;
+    }
 
+    public function getQueryString(array $params) : string
+    {
+        $fieldFilterParams = [];
+    
+        foreach ($params as $filterKey => $filterValue) {
+            $fieldFilterParams[$filterKey] = $this->getFilterParams->execute($filterKey, $filterValue);
+        }
     }
 }
